249580bb57a73f380417a7adb2d0ea00125d6b94
Karina <ka_selingardi@hotmail.com>
2024-04-02 10:09:14 -0300

    new commit

--- src/Exemplo0302/exemplo0302.java
+++ src/Exemplo0302/exemplo0302.java
@@ -1,2 +1,34 @@
-package Exemplo0302;public class exemplo0302 {
+package Exemplo0302;
+import javax.swing.*;
+public class exemplo0302
+{
+public static void main(String args[]){
+    String aux = JOptionPane.showInputDialog(null,"Forneça um numero inteiro entr 0 e 9: ");
+    if(aux != null) // verifica se o usuario entrou com um valor por meio da caixa de diálogo
+    {
+        try {
+            int x = Integer.parseInt(aux);
+            String extenso = "";
+            switch (x)
+            {
+                case 0: extenso = "zero"; break;
+                case 1: extenso = "um"; break;
+                case 2: extenso = "dois"; break;
+                case 3: extenso = "tres"; break;
+                case 4: extenso = "quatro"; break;
+                case 5: extenso = "cinco"; break;
+                case 6: extenso = "seis"; break;
+                case 7: extenso = "sete"; break;
+                case 8: extenso = "oito"; break;
+                case 9: extenso = "nove"; break;
+                default: extenso = "número desconhecido";
+
+            }
+            JOptionPane.showMessageDialog(null,"Número fornecido: " + extenso);
+        }
+        catch (NumberFormatException erro){
+            JOptionPane.showMessageDialog(null,"Digite apenas valores numéricos inteiro!");
+        }
+    }
+}
 }
--- src/Exemplo0303/Exemplo0303.java
+++ src/Exemplo0303/Exemplo0303.java
@@ -1,2 +1,36 @@
-package Exemplo0303;public class Exemplo0303 {
+
+package Exemplo0303;
+import javax.swing.*;
+public class Exemplo0303 {
+    public static void main (String args[])
+    {
+        String auxl = JOptionPane.showInputDialog(null, "foneça o valor do primero número");
+        if (auxl == null)
+            System.exit(0);
+        String aux2 = JOptionPane.showInputDialog(null,"forneça o valor do segundo número");
+        if (aux2 == null)
+            System.exit(0);
+        try{
+            int num1 = Integer.parseInt(auxl);
+            int num2 = Integer.parseInt(aux2);
+            JOptionPane.showMessageDialog(null,"Soma =" +( num1 + num2));
+            JOptionPane.showMessageDialog(null," Subtração= " + (num1 - num2));
+            JOptionPane.showMessageDialog(null," multiplicação = " + (num1 * num2));
+            JOptionPane.showMessageDialog(null," Divisão = " + (num1 / num2));
+        }
+        catch (ArithmeticException erro){
+            JOptionPane.showMessageDialog(null,"Erro na divisão por zero!", "Erro",JOptionPane.ERROR_MESSAGE);
+
+        }
+        catch (NumberFormatException erro)
+        {
+            JOptionPane.showMessageDialog(null,"Digite apenas números inteiros","Erro", JOptionPane.ERROR_MESSAGE);
+
+        }
+    finally
+        {
+            JOptionPane.showMessageDialog(null,"Final da execução!");
+        }
+        System.exit(0);
+    }
 }
